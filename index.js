ed2afe3b862204a93a86f4786ff56593c53a6a9b
gelito <angel.quesada@kubide.es>
2017-04-12 10:35:34 +0200

    Define Class and Object

---
+++ index.js
@@ -0,0 +1,10 @@
+const debug = require('debug')('es6-classes:main');
+
+// Crear una clase Person que tenga: nombre y apellidos, edad, DNI, profesion
+const Person = require('./models/person.js');
+debug("Person Class:", Person);
+
+const person = new Person();
+debug("person Object:", person);
+debug("typeof Object:", typeof person );
+debug("person is an instance of the class Person?", person instanceof Person);
